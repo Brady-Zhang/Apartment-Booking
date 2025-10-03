ea8231412585bff2ff15fb604d948dc0e3edc703
depeng zhang <zhangdepeng3@gmail.com>
2025-10-03 11:23:25 +1000

    chore: restructure solution into src and test folders

---
+++ src/Bookify.Domain/Apartments/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBook.Domain.Apartments
+{
+    public record Address(
+        string Country,
+        string State,
+        string City,
+        string ZipCode,
+        string Street);
+}
---
+++ src/Bookify.Domain/Apartments/Amenity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBook.Domain.Apartments
+{
+    public enum Amenity
+    {
+        Wifi = 1,
+        AirConditioning = 2,
+        Parking = 3,
+        SwimmingPool = 4,
+        Gym = 5,
+        PetFriendly = 6,
+        Spa = 7,
+        Terrace = 8,
+        MountainView = 9,
+        GardenView = 10
+    }
+}
